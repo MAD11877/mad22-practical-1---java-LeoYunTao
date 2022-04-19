d6f0599a926d3e01025db6990447cde8d7c3e4dd
YT <s10219150@connect.np.edu.sg>
2022-04-19 10:54:38 +0800

    attempt 11

--- Question2.java
+++ Question2.java
@@ -7,11 +7,11 @@ public class Question2
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
 
-    Scanner in2 = new Scanner(System.in);
-    double weight = in2.nextDouble();
+    // Scanner in2 = new Scanner(System.in);
+    // double weight = in2.nextDouble();
 
-    double bmi = weight / (height * height);
+    // double bmi = weight / (height * height);
     
-    System.out.println(bmi);
+    // System.out.println(bmi);
   }
 }
